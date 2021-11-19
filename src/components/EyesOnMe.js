5e2cd3720e0c6ef2b4837d698590c7a74ac2da17
David <davidchedrick@gmail.com>
2021-11-18 23:05:05 -0600

    commit tree 212b7c42430cc1d01b8d6c0417ee0ae753d92e38

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+const sayThis = (phrase) => console.log(phrase)
+
+
+function EyesOnMe() {
+  return (
+     
+    <button 
+        onFocus={() => sayThis('Good!')} 
+        onBlur={() => sayThis('Hey! Eyes on me!')}
+    >
+        Eyes on me
+    </button>
+    
+    )
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+import React from 'react';
+
+const sayThis = (phrase) => console.log(phrase)
+
+function Keypad() {
+  return <input type="password" onChange={() => sayThis('Entering password...')} />
+}
+
+
+export default Keypad;
